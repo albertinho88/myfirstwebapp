20d5fe2d5baacb78038ccbf2479ee578ddf44da0
Jlarragamena <jlarragamena@trueblue.com>
2024-06-25 18:18:38 -0500

    Creating a Login Form

--- src/main/java/ec/clicka/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/ec/clicka/springboot/myfirstwebapp/login/LoginController.java
@@ -1,17 +1,14 @@
 package ec.clicka.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        System.out.println("Request param is " + name);
+    public String goToLoginPage() {
+//        model.put("name", name);
         return "login";
     }
 
