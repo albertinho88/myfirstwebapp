4fce0462ceefb5f880478bb814995e98d2689795
Jlarragamena <jlarragamena@trueblue.com>
2024-06-27 00:19:18 -0500

    Refactoring and removing hardcoding of User Id

--- src/main/java/ec/clicka/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/ec/clicka/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,6 +1,8 @@
 package ec.clicka.springboot.myfirstwebapp.todo;
 
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -25,14 +27,15 @@ public class TodoController {
 
     @RequestMapping("list-todos")
     public String listAllTodos(ModelMap model) {
-        List<Todo> todos = todoService.findByUsername("in28minutes");
+        String username = getLoggedInUsername();
+        List<Todo> todos = todoService.findByUsername(username);
         model.put("todos", todos);
         return "listTodos";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
     public String showNewTodoPage(ModelMap model) {
-        String username = (String)model.get("name");
+        String username = getLoggedInUsername();
         Todo todo = new Todo(0, username, "", LocalDate.now(), false);
         model.put("todo", todo);
         return "todo";
@@ -44,7 +47,7 @@ public class TodoController {
             return "todo";
         }
 
-        String username = (String)model.get("name");
+        String username = getLoggedInUsername();
         todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
         return "redirect:list-todos";
     }
@@ -69,6 +72,11 @@ public class TodoController {
         }
         todoService.updateTodo(todo);
         return "redirect:list-todos";
+    }
+
+    private static String getLoggedInUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 
 }
